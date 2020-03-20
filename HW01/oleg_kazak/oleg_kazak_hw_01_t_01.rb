e6448725ce8705500a1fbe0401ccecd84f1586a0
Kazak Aleh <q4wk13@gmail.com>
2020-03-21 01:13:56 +0300

    degrees the hour hand passes in a minute

---
+++ HW01/oleg_kazak/oleg_kazak_hw_01_t_01.rb
@@ -0,0 +1,7 @@
+print 'Enter the value of the hours hand: '
+  hours = gets.to_i
+print 'Enter the value of the minutes hand: '
+  minutes = gets.to_i
+def angle_time(hours, minutes)
+# Degrees the hour hand passes in a minute
+   hour_hand = 1 / (12 * 60.0) * 360
