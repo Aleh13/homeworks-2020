4e781fc74efb22913f3588b47d8dcec902ff88d9
Kazak Aleh <q4wk13@gmail.com>
2020-03-21 10:58:38 +0300

    error correction robocop

--- HW01/oleg_kazak/oleg_kazak_hw_01_t_01.rb
+++ HW01/oleg_kazak/oleg_kazak_hw_01_t_01.rb
@@ -1,18 +1,18 @@
 print 'Enter the value of the hours hand: '
-  hours = gets.to_i
+hours = gets.to_i
 print 'Enter the value of the minutes hand: '
-  minutes = gets.to_i
+minutes = gets.to_i
 def angle_time(hours, minutes)
 # Degrees the hour hand passes in a minute
-   hour_hand = 1 / (12 * 60.0) * 360
+  hour_hand = 1 / (12 * 60.0) * 360
 # Degrees the minute hand for a minute
-   minutes_hand = 360 * 1 / 60
+  minutes_hand = 360 * 1 / 60
 # Clockwise angle
-   hour_angle = hour_hand * (minutes + hours * 60)
+  hour_angle = hour_hand * (minutes + hours * 60)
 # Minute hand angle
-   minutes_angle = minutes_hand * minutes
+  minutes_angle = minutes_hand * minutes
 # The angle between the hour and minute hand
-   (hour_angle - minutes_angle).abs
+  (hour_angle - minutes_angle).abs
 end
 
-puts "#{angle_time(hours, minutes)}"
+puts "The angle is #{angle_time(hours, minutes)}"
