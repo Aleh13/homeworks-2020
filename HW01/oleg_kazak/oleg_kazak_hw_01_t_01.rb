1bb8b71a4bd6b185a1341a1bdb6c94c787885dc2
Aleh13 <52246740+Aleh13@users.noreply.github.com>
2020-03-22 23:37:17 +0300

    Update oleg_kazak_hw_01_t_01.rb
    correction of variable names

--- HW01/oleg_kazak/oleg_kazak_hw_01_t_01.rb
+++ HW01/oleg_kazak/oleg_kazak_hw_01_t_01.rb
@@ -3,10 +3,10 @@ hours = gets.to_i
 print 'Enter the value of the minutes hand: '
 minutes = gets.to_i
 def angle_time(hours, minutes)
-  clockwise_angle = 0.5 * (minutes + hours * 60)
-  minute_hand_angle = 6 * minutes
+  HOUR_HAND = 0.5 * (minutes + hours * 60)
+  MINUTES_HAND = 6 * minutes
   # The angle between the hour and minute hand
-  (clockwise_angle - minute_hand_angle).abs
+  (HOUR_HAND - MINUTES_HAND).abs
 end
 
 puts "The angle is #{angle_time(hours, minutes)}"
