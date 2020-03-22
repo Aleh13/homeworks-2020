7fe912130cb80cb94d0724bda1ba9f3e9a89970a
Kazak Aleh <q4wk13@gmail.com>
2020-03-22 09:59:09 +0300

    program fix

--- HW01/oleg_kazak/oleg_kazak_hw_01_t_01.rb
+++ HW01/oleg_kazak/oleg_kazak_hw_01_t_01.rb
@@ -3,16 +3,10 @@ hours = gets.to_i
 print 'Enter the value of the minutes hand: '
 minutes = gets.to_i
 def angle_time(hours, minutes)
-  # Degrees the hour hand passes in a minute
-  hour_hand = 1 / (12 * 60.0) * 360
-  # Degrees the minute hand for a minute
-  minutes_hand = 360 * 1 / 60
-  # Clockwise angle
-  hour_angle = hour_hand * (minutes + hours * 60)
-  # Minute hand angle
-  minutes_angle = minutes_hand * minutes
+  clockwise_angle = 0.5 * (minutes + hours * 60)
+  minute_hand_angle = 6 * minutes
   # The angle between the hour and minute hand
-  (hour_angle - minutes_angle).abs
+  (clockwise_angle - minute_hand_angle).abs
 end
 
 puts "The angle is #{angle_time(hours, minutes)}"
